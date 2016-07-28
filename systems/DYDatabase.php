caea0d51d3e021064d051953a018784f5208b4d6
diligentyang <905407339@qq.com>
2016-07-28 22:51:34 +0800

    执行PDO

--- systems/DYDatabase.php
+++ systems/DYDatabase.php
@@ -4,9 +4,10 @@ defined('ACCESS') OR exit('No direct script access allowed');
 class DYDatabase
 {
     public $pdo;
+
     function __construct()
     {
-        $this->pdo=$this->createPDO();
+        $this->pdo = $this->createPDO();
 
     }
 
@@ -28,17 +29,43 @@ class DYDatabase
         $db_user = $db['username'];
         $db_pass = $db['password'];
         $dsn = "mysql:dbname=$dbname;host=$host";
-        try{
-            $pdo=new PDO($dsn,$db_user,$db_pass);
-        }catch(PDOException $e){
-            echo '数据库连接失败'.$e->getMessage();
+        try {
+            $pdo = new PDO($dsn, $db_user, $db_pass);
+        } catch (PDOException $e) {
+            echo '数据库连接失败' . $e->getMessage();
             exit();
         }
         return $pdo;
     }
 
-    function query()
+    function query($sql = "", $mode = "array")
     {
-        echo "query";
+        $sql = trim($sql);
+        if ($sql == "") {
+            showErrors("the mothe query neet at least one param!");
+        }
+        $query = $this->pdo->query($sql);
+        if (!query) {
+            showErrors("the sql string is false");
+        }
+        if (!strpos(strtolower($sql), "select")) {
+            return $query;
+        }
+
+        switch ($mode) {
+            case 'array' :
+                $res = $query->fetch(PDO::FETCH_ASSOC);
+                break;
+            case 'object' :
+                $res = $query->fetchObject();
+                break;
+            case 'count':
+                $res = $query->rowCount();
+                break;
+            default:
+                showErrors("SQLERROR: please check your second param!");
+        }
+
+
     }
 }
