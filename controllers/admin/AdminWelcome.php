938a879657ae84d543287b8d90206819918c14c8
diligentyang <905407339@qq.com>
2016-07-28 14:06:43 +0800

    使控制器支持多级目录

---
+++ controllers/admin/AdminWelcome.php
@@ -0,0 +1,11 @@
+<?php
+class AdminWelcome{
+	
+	public function index()
+	{
+		echo "welcome to admin/AdminWelcome";
+	}
+	
+}
+
+?>
--- systems/DYApp.php
+++ systems/DYApp.php
@@ -15,38 +15,93 @@ class DYApp
         $this->runController();
     }
 
+    /**
+     * 不支持控制器多级目录
+     */
+//    public function runController()
+//    {
+//        $routes = getRouteAll();
+//        $controllerName="";
+//        if($routes){
+//            $routesArray = explode("/",$routes);
+//            $num = count($routesArray);
+//            foreach($routesArray as $value){
+//                $num--;
+//                $controllerPath = "controllers/".lcfirst($value).".php";
+//                if(!is_file(BASE_PATH.$controllerPath)){
+//                    $controllerPath = "controllers/".ucfirst($value).".php";
+//                    if(!is_file(BASE_PATH.$controllerPath)) {
+//                        if($num==0){
+//                            showErrors("Can't find the Controller!");
+//                        }
+//                        continue;
+//                    }
+//                    $controllerName = $value;
+//                    break;
+//                }
+//                $controllerName = $value;
+//                break;
+//            }
+//        }else{
+//            $controllerName = DEFAULT_CONTROLLER;
+//            $controllerPath = "controllers/".DEFAULT_CONTROLLER.".php";
+//        }
+//        //先默认，控制器没有多级文件夹，不使用strrpos确定的原因是，如果为多级文件，文件名和控制器名字相同，易出现bug
+//        importFile($controllerPath);
+//        $controller = new $controllerName();
+//        $routes = substr($routes,strlen($controllerName)+1);
+//        //如果$routes 为false，则说明URL中不含method
+//        if($routes){
+//            $methodArray = explode("/",$routes);
+//            if(method_exists($controller,$methodArray[0])){
+//                $method = $methodArray[0];
+//            }else{
+//                showErrors("Can't find the method!");
+//            }
+//        }else{
+//            $method = DEFAULT_METHOD;
+//        }
+//        $controller->$method();
+//
+//    }
+
+    /**
+     * 支持控制器多级目录
+     */
     public function runController()
     {
         $routes = getRouteAll();
         $controllerName="";
+        $controllerRoute="";
         if($routes){
             $routesArray = explode("/",$routes);
             $num = count($routesArray);
             foreach($routesArray as $value){
                 $num--;
-                $controllerPath = "controllers/".lcfirst($value).".php";
+                if($controllerRoute==""){
+                    $controllerRoute = $value;
+                }else{
+                    $controllerRoute.= "/".$value;
+                }
+                $controllerPath = "controllers/".$controllerRoute.".php";
                 if(!is_file(BASE_PATH.$controllerPath)){
-                    $controllerPath = "controllers/".ucfirst($value).".php";
-                    if(!is_file(BASE_PATH.$controllerPath)) {
-                        if($num==0){
-                            showErrors("Can't find the Controller!");
-                        }
-                        continue;
+                    if($num==0){
+                        showErrors("Can't find the Controller!");
                     }
-                    $controllerName = $value;
-                    break;
+                    continue;
                 }
                 $controllerName = $value;
                 break;
             }
         }else{
             $controllerName = DEFAULT_CONTROLLER;
+            $controllerRoute = DEFAULT_CONTROLLER;
             $controllerPath = "controllers/".DEFAULT_CONTROLLER.".php";
         }
         //先默认，控制器没有多级文件夹，不使用strrpos确定的原因是，如果为多级文件，文件名和控制器名字相同，易出现bug
         importFile($controllerPath);
         $controller = new $controllerName();
-        $routes = substr($routes,strlen($controllerName)+1);
+        $routes = substr($routes,strlen($controllerRoute)+1);
         //如果$routes 为false，则说明URL中不含method
         if($routes){
             $methodArray = explode("/",$routes);
@@ -59,7 +114,7 @@ class DYApp
             $method = DEFAULT_METHOD;
         }
         $controller->$method();
-
     }
+
 
 }
