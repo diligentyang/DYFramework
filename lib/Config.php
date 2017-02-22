ab855948612c3c3732dc7e4018ecd8ef4c9983a9
diligentyang <905407339@qq.com>
2017-02-22 21:10:57 +0800

    add lib file and Config.php

---
+++ lib/Config.php
@@ -0,0 +1,39 @@
+<?php
+namespace lib;
+
+class Config implements \ArrayAccess
+{
+    protected $path;
+    protected $configs = array();
+
+    function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    function offsetGet($key)
+    {
+        if(empty($this->configs[$key]))
+        {
+            $file_path = $this->path.'/'.$key.'.php';
+            $config = include $file_path;
+            $this->configs[$key] = $config;
+        }
+        return $this->configs[$key];
+    }
+
+    function offsetExists($key)
+    {
+        return isset($this->configs[$key]);
+    }
+
+    function offsetUnset($key)
+    {
+        unset($this->configs[$key]);
+    }
+
+    function offsetSet($key, $value)
+    {
+        throw new \Exception("Cannot write config file.");
+    }
+}
