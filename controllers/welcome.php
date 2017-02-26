385843c01f1d2214513ec3da359ad20fad074030
diligentyang <905407339@qq.com>
2017-02-26 20:38:37 +0800

    init

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -5,6 +5,11 @@ class Welcome extends \systems\DYController
 {
     public function actionIndex()
     {
+        $this->view("index");
+    }
+
+    function actionTest()
+    {
         $data = array("id"=>'1',"name"=>'ysy');
 
         $model = $this->model('TestModel');
@@ -13,12 +18,5 @@ class Welcome extends \systems\DYController
         $passwd = new \lib\Password();
         echo $passwd->generatePasswordHash("222222");
         $this->view("request", $data);
-
-
-    }
-
-    function actionTest()
-    {
-        echo "welcome test";
     }
 }
