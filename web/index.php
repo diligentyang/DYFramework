2faa9ba277c2fdd86b492d51c85fe2ed1e48db91
diligentyang <905407339@qq.com>
2017-06-07 14:17:02 +0800

    add composer

--- web/index.php
+++ web/index.php
@@ -18,5 +18,21 @@ defined("ACCESS") or define("ACCESS", true);
  */
 require_once "../systems/DYConstant.php"; //常量定义
 require_once "../systems/Autoload.php";
+require_once "../vendor/autoload.php";
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
 spl_autoload_register('\systems\Autoload::loader');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('framworkLog');
+$log->pushHandler(new StreamHandler(BASE_PATH.'/log/framelog.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('警告：鲁班大师');
+$log->error('Bar');
+echo "1111111";
 systems\DYBase::init();
