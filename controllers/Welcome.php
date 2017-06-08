e6a575bf354c49597c74e24921ec1c9cc2c545ac
diligentyang <905407339@qq.com>
2017-06-08 17:47:01 +0800

    update upload

--- controllers/Welcome.php
+++ controllers/Welcome.php
@@ -67,4 +67,52 @@ EOF;
         $data = ['a'=>1,'b'=>2];
         $this->RenderView("hello",$data);
     }
+
+    //文件上传
+    function actionUpload()
+    {
+        if(\lib\Request::isPost()){
+            $storage = new \Upload\Storage\FileSystem(BASE_PATH.'upload');
+            $file = new \Upload\File('foo', $storage);//foo和前端表单的id对应
+
+            // Optionally you can rename the file on upload
+            $new_filename = uniqid();
+            $file->setName($new_filename);
+
+            // Validate file upload
+            // MimeType List => http://www.iana.org/assignments/media-types/media-types.xhtml
+            $file->addValidations(array(
+                // Ensure file is of type "image/png"
+                new \Upload\Validation\Mimetype('image/png'),
+
+                //You can also add multi mimetype validation
+                //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
+
+                // Ensure file is no larger than 5M (use "B", "K", M", or "G")
+                new \Upload\Validation\Size('2M')
+            ));
+
+            // Access data about the file that has been uploaded
+            $data = array(
+                'name'       => $file->getNameWithExtension(),
+                'extension'  => $file->getExtension(),
+                'mime'       => $file->getMimetype(),
+                'size'       => $file->getSize(),
+                'md5'        => $file->getMd5(),
+                'dimensions' => $file->getDimensions()
+            );
+
+            // Try to upload file
+            try {
+                // Success!
+                $file->upload();
+                dump($data);
+            } catch (\Exception $e) {
+                // Fail!
+                $errors = $file->getErrors();
+                dump($errors);
+            }
+        }
+        $this->view("upload");
+    }
 }
---
+++ views/upload.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>文件上传测试</title>
+</head>
+<body>
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="foo" value=""/>
+        <input type="submit" value="Upload File"/>
+    </form>
+</body>
+</html>
