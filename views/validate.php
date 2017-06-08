b290510d2af50e4da9045fad9d7a6ca1dc1656b9
diligentyang <905407339@qq.com>
2017-06-08 21:08:37 +0800

    update

--- views/validate.php
+++ views/validate.php
@@ -5,8 +5,8 @@
 </head>
 <body>
 <form method="POST">
-    <input type="text" name="username" value=""/>
-    <input type="text" name="email" value=""/>
+    <input type="text" name="username" value="" placeholder="用户名"/>
+    <input type="text" name="email" value="" placeholder="邮箱" />
     <input type="submit" value="提交"/>
 </form>
 </body>
